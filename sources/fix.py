c84d30e27d48c7d7fa8c2f8140bcd944dd55520e
Marc Foley <m.foley.88@gmail.com>
2021-03-03 11:36:43 +0000

    Make vert metrics match Roboto Sans

--- sources/fix.py
+++ sources/fix.py
@@ -5,17 +5,26 @@ sources = glob("*.ufo")
 
 for source in sources:
     source = Font(source)
-#    if not source.info.openTypeOS2Selection:
-#        source.info.openTypeOS2Selection = []
-#    if 7 not in source.info.openTypeOS2Selection:
-#        source.info.openTypeOS2Selection.append(7)
-    delattr(source.info, "openTypeNameLicense")
 
-#    source.info.openTypeOS2TypoAscender = 1900
-#    source.info.openTypeOS2TypoLineGap = -500
-#    source.info.openTypeOS2TypoDescender = 0
-#
-#    source.info.openTypeHheaAscender = 1900
-#    source.info.openTypeHheaDescender = -500
-#    source.info.openTypeHheaLineGap = 0
+    if not source.info.openTypeOS2Selection:
+        source.info.openTypeOS2Selection = []
+    if 7 not in source.info.openTypeOS2Selection:
+        source.info.openTypeOS2Selection.append(7)
+
+    if "openTypeOS2CodePageRanges":
+        delattr(source.info, "openTypeOS2CodePageRanges")
+
+    if "openTypeNameLicense":
+        delattr(source.info, "openTypeNameLicense")
+
+    if "openTypeOS2UnicodeRanges":
+        delattr(source.info, "openTypeOS2UnicodeRanges")
+
+    source.info.openTypeOS2TypoAscender = 750
+    source.info.openTypeOS2TypoDescender = -250
+    source.info.openTypeOS2TypoLineGap = 50
+
+    source.info.openTypeHheaAscender = 750
+    source.info.openTypeHheaDescender = -250
+    source.info.openTypeHheaLineGap = 50
     source.save()
